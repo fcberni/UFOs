d3765784e74dfc530c86c3a1bd02635cfb5b2d7d
Frank <fcberni@hotmail.com>
2020-12-13 20:47:57 -0500

    Finishing touches on Java

--- static/js/app.js
+++ static/js/app.js
@@ -59,17 +59,22 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    d3.selectAll("input").on("change", updateFilters);
-  
+    let filteredData = tableData;
+    console.log(filteredData)
+    
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    Object.entries(filters).forEach(([key,value]) => {
+    filteredData = filteredData.filter(row=>row[key]===value);
+    });
   
     // 10. Finally, rebuild the table using the filtered data
+    buildTable(filteredData);
     
-  }
+  };
   
   // 2. Attach an event to listen for changes to each filter
+  d3.selectAll("input").on("change", updateFilters);
   
   
   // Build the table when the page loads
